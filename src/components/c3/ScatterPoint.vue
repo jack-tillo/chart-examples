354101a9dd4d0d9bfb395b9312a4498554a19fb8
Jack Chapman <jack@reward.cloud>
2020-04-07 17:15:17 +0100

    Add scatter point c3 graph

--- src/components/c3/ScatterPoint.vue
+++ src/components/c3/ScatterPoint.vue
@@ -1,13 +1,84 @@
 <template>
-    <div>Scatter Point</div>
+    <div id="my-chart2"></div>
 </template>
 
 <script>
+import c3 from 'c3'
 export default {
-    
+    props: {
+        graphData: Array
+    },
+    data() {
+        return {
+            chart: {}
+        }
+    },
+    watch: {
+        graphData(val) {
+            this.chart.load({
+                columns: [
+                    ...val
+                ]
+            })
+        }
+    },
+    mounted() {
+        this.chart = c3.generate({
+            bindto: '#my-chart2',
+            size: {
+                height: 350,
+                width: 800
+            },
+            data: {
+                x: 'x',
+                columns: [
+                    ['x', 0, 1, 1, 2, 3, 4, 5, 6, 7, 5, 2, 5, 6, 2, 6, 7, 2, 0, 2 ,6, 2, 6, 2, 6, 8, 3, 3, 6, 7, 3],
+                ['', 0, 1, 1, 2, 3, 3, 3, 4, 1, 2, 4, 6, 2, 6, 5, 3, 2, 6, 3, 7, 9, 0, 3, 7, 8, 3, 7, 8, 3, 7]
+                ],
+                type: 'scatter',
+            },
+            axis: {
+                x: {
+                    type: 'category',
+                    categories: ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun']
+                },
+                y: {
+                    label: 'Time of day',
+                    format: x => `${x} am`
+                }
+            },
+            grid: {
+                x: {
+                    show: true
+                }
+            }
+        })
+        this.chart.element = this.$refs['chart-container']
+    }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+#line1 {
+    width: 800px;
+    height: 350px;
+}
 
+.c3-lines,
+.domain {
+    fill: transparent;
+}
+
+.c3-xgrid {
+    stroke-width: 1px;
+    stroke: #dddcdc;
+    &:first-of-type,
+    &:last-of-type {
+        stroke: none;
+    }
+}
+
+.c3-xgrid-focus {
+    stroke: #adadad;
+}
 </style>
--- src/views/C3.vue
+++ src/views/C3.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container">
-    <h1>c3.js Charts</h1>
-    <button @click="loadC3Data">Load data</button>
-    <c3-line-graph :graph-data="lineData"/>
-    <c3-scatter-point/>
-  </div>
+    <div class="container">
+        <h1>c3.js Charts</h1>
+        <button @click="loadC3Data">Load data</button>
+        <c3-line-graph :graph-data="lineData" />
+        <c3-scatter-point :graph-data="scatterData" />
+    </div>
 </template>
 
 <script>
@@ -18,12 +18,21 @@ export default {
     },
     data() {
         return {
-            lineData: []
+            lineData: [],
+            scatterData: []
         }
     },
     methods: {
-        loadC3Data () {
-            this.lineData = [['Revenue', 30, 200, 100, 400, 150, 250, 120, 400], ['Spend', 130, 340, 200, 500, 250, 350, 320, 500], ['Goal', 200, 200, 240, 240, 250, 260, 200, 300]]
+        loadC3Data() {
+            this.lineData = [
+                ['Revenue', 30, 200, 100, 400, 150, 250, 120, 400],
+                ['Spend', 130, 340, 200, 500, 250, 350, 320, 500],
+                ['Goal', 200, 200, 240, 240, 250, 260, 200, 300]
+            ]
+            this.scatterData = [
+                ['one_x', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+                ['one', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+            ]
         }
     }
 }
